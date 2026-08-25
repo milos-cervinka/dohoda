99b0a2c163fd39f4941a84cb5d52dc4b2fbd7744
milos-cervinka <cervimi24@sps-prosek.cz>
2026-08-25 14:47:49 +0200

    Setup routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import Login from './assets/components/login'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './assets/components/login';
+import Register from './assets/components/register';
 
-import './index.css'
-
-function App() {
+export default function App() {
   return (
-    <Login/ >
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<h1>404: Stránka nenalezena</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
--- src/assets/components/register.tsx
+++ src/assets/components/register.tsx
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Register = () => {
   return (
     <div className="flex min-h-screen items-center justify-center px-4 bg-gray-800">
       <div className="w-full max-w-md p-6 border-2 border-olive-300 rounded-lg shadow-md flex items-center justify-center flex-col gap-4">
-        <h1 className="text-2xl font-bold text-olive-300">Přihlášení</h1>
+        <h1 className="text-2xl font-bold text-olive-300">Registrace</h1>
         <form className="w-full flex flex-col gap-4">
           <input
             type="text"
@@ -16,14 +16,19 @@ const Login = () => {
             placeholder="Password"
             className="text-olive-300 placeholder:text-olive-500 w-full px-4 py-2 border border-olive-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
+          <input
+            type="password"
+            placeholder="Repeat password"
+            className="text-olive-300 placeholder:text-olive-500 w-full px-4 py-2 border border-olive-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
           <button
             type="submit"
             className="w-full bg-red-800 text-white py-2 rounded-md hover:bg-blue-600 transition-colors duration-300"
           >
-            Login
+            Register
           </button>
           <p>
-            <Link to="/register">Don't have an account? Register</Link>
+            <Link to="/login">Already have an account? Login</Link>
           </p>
         </form>
       </div>
@@ -31,4 +36,4 @@ const Login = () => {
   )
 }
 
-export default Login
+export default Register
