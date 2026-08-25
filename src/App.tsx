d4455bdb796dfba27bba5395ddf80034b4a99e43
milos-cervinka <cervimi24@sps-prosek.cz>
2026-08-25 13:04:33 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Login from './assets/components/login'
+
+import './index.css'
+
+function App() {
+  return (
+    <Login/ >
+  )
+}
+
+export default App
---
+++ src/assets/components/login.tsx
@@ -0,0 +1,29 @@
+const Login = () => {
+  return (
+    <div className="flex min-h-screen items-center justify-center px-4 bg-gray-800">
+      <div className="w-full max-w-md p-6 border-2 border-olive-300 rounded-lg shadow-md flex items-center justify-center flex-col gap-4">
+        <h1 className="text-2xl font-bold text-olive-300">Přihlášení</h1>
+        <form className="w-full flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Username"
+            className="text-olive-300 placeholder:text-olive-500 w-full px-4 py-2 border border-olive-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="text-olive-300 placeholder:text-olive-500 w-full px-4 py-2 border border-olive-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+          />
+          <button
+            type="submit"
+            className="w-full bg-red-800 text-white py-2 rounded-md hover:bg-blue-600 transition-colors duration-300"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
